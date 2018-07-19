5182d00818a40e5e8a9d00521c270b6ef8d88421
Lorena <Lorenija@gmail.com>
2018-07-19 13:45:34 +0100

    base html set up

--- concept_photography/urls.py
+++ concept_photography/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from . import views as homeViews
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^', homeViews.index),
 ]
