a5900acde9d0ba9ecd150a68b477996a836d8a13
Lorena <Lorenija@gmail.com>
2018-08-12 19:00:37 +0300

    pricing.html content

--- pricing_page/views.py
+++ pricing_page/views.py
@@ -2,10 +2,14 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
 from contact_information.models import ContactInfo
+from pricing_page.models import PricingFrontPage
+from pricing_page.models import Pricing
 
 
 def index(request):
     template = loader.get_template('pricing.html')
-    context = {'contactInfo': ContactInfo.objects.first()}
+    context = {'contactInfo': ContactInfo.objects.first(),
+               'PricingFrontPage': PricingFrontPage.objects.first(),
+               'Pricing': Pricing.objects.first()}
 
     return HttpResponse(template.render(context, request))
