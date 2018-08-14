bc36e4a3eda1a7a96f289debeb8699205ec895ad
Lorena <Lorenija@gmail.com>
2018-08-14 12:32:44 +0300

    services.html content

--- services_page_content/models.py
+++ services_page_content/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class ServicesFrontPage(models.Model):
     main_title = models.CharField(max_length=200)
-    description_field = models.CharField(max_length=200)
+    description_field = models.CharField(max_length=500)
 
 
 class Service(models.Model):
     title = models.CharField(max_length=200)
-    description_field = models.CharField(max_length=200)
+    description_field = models.CharField(max_length=600)
     image = models.FileField(upload_to='assets/images')
--- services_page_content/views.py
+++ services_page_content/views.py
@@ -2,10 +2,17 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
 from contact_information.models import ContactInfo
+from services_page_content.models import ServicesFrontPage
+from services_page_content.models import Service
 
 
 def index(request):
+
+    shouldShowServices = len(Service.objects.all()) >= 1
+
     template = loader.get_template('services.html')
-    context = {'contactInfo': ContactInfo.objects.first()}
+    context = {'contactInfo': ContactInfo.objects.first(),
+               'ServicesFrontPage': ServicesFrontPage.objects.first(),
+               'Service': Service.objects.all()}
 
     return HttpResponse(template.render(context, request))
