440f3d5334791a7bca2e4c00fadd21f6e4da6b01
Lorena <Lorenija@gmail.com>
2018-08-14 11:36:37 +0300

    services.html url set up

--- concept_photography/urls.py
+++ concept_photography/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 from . import views as homeViews
 from about_us_page_content import views as aboutViews
 from pricing_page import views as pricingViews
+from services_page_content import views as servicesViews
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
@@ -29,6 +30,7 @@ urlpatterns = [
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^about', aboutViews.index),
     url(r'^pricing', pricingViews.index),
+    url(r'^services', servicesViews.index),
     url(r'^', homeViews.index),
 ]
 
