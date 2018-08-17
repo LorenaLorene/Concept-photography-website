df8779d6bed8fe48f3cfa3383837c519402b2d28
Lorena <Lorenija@gmail.com>
2018-08-17 13:49:01 +0300

    gdpr.html content

--- gdpr_page_content/views.py
+++ gdpr_page_content/views.py
@@ -2,11 +2,13 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
 from contact_information.models import ContactInfo
+from gdpr_page_content.models import Gdpr
 
 
 def index(request):
 
     template = loader.get_template('gdpr.html')
-    context = {'contactInfo': ContactInfo.objects.first()}
+    context = {'contactInfo': ContactInfo.objects.first(),
+               'Gdpr': Gdpr.objects.first()}
 
     return HttpResponse(template.render(context, request))
