9902cbd359f8bbcc0cf7941a7c378537bbdf1b98
Lorena <Lorenija@gmail.com>
2018-08-17 12:56:20 +0300

    faqs.html content

--- faqs_page_content/views.py
+++ faqs_page_content/views.py
@@ -2,11 +2,13 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
 from contact_information.models import ContactInfo
+from faqs_page_content.models import PhotographyFaq
 
 
 def index(request):
 
     template = loader.get_template('faqs.html')
-    context = {'contactInfo': ContactInfo.objects.first()}
+    context = {'contactInfo': ContactInfo.objects.first(),
+               'PhotographyFaqs': PhotographyFaq.objects.all()}
 
     return HttpResponse(template.render(context, request))
