e2db461176a76fe98be1e73aff9772a3d8b6476b
Lorena <Lorenija@gmail.com>
2018-08-17 11:02:39 +0300

    gallery.html content

--- gallery_content_page/views.py
+++ gallery_content_page/views.py
@@ -2,10 +2,14 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
 from contact_information.models import ContactInfo
+from gallery_content_page.models import GalleryFrontPage
+from gallery_content_page.models import GalleryImage
 
 
 def index(request):
     template = loader.get_template('gallery.html')
-    context = {'contactInfo': ContactInfo.objects.first()}
+    context = {'contactInfo': ContactInfo.objects.first(),
+               'GalleryFrontPage': GalleryFrontPage.objects.first(),
+               'GalleryImage': GalleryImage.objects.all()}
 
     return HttpResponse(template.render(context, request))
